9aa141e0b30636907295439f4e0c64fc042da042
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-06-05 15:47:16 -0300

    first commit

---
+++ src/main/java/com/fernando/fafoodapi/FaFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.fafoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaFoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/fafoodapi/FaFoodApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fernando.fafoodapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class FaFoodApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
